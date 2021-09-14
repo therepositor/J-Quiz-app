6ddd384394aa18cd83b8bd91daf7fe916f8a160c
Joseph Oguntebi <joseph.oguntebi@yahoo.com>
2021-09-14 03:37:21 +0100

    first question layout

---
+++ app.js
@@ -0,0 +1,50 @@
+const quizData = [
+    {
+       Question: '',
+       a: '',
+       b: '',
+       c: '',
+       d: '',
+       e: ''
+    },
+    {
+        Question: 'Pick a color',
+        a: '',
+        b: '',
+        c: '',
+        d: '',
+        e: ''
+    },
+    {
+        Question: 'If you were a ghost, how would you haunt someone?',
+        a: '',
+        b: '',
+        c: '',
+        d: '',
+        e: ''
+    },
+    {
+        Question: 'What do you do when you\'re stressed out?',
+        a: '',
+        b: '',
+        c: '',
+        d: '',
+        e: ''
+    },
+    {
+        Question: 'Pick an African country to visit',
+        a: '',
+        b: '',
+        c: '',
+        d: '',
+        e: ''
+    },
+    {
+        Question: 'Pick a snack',
+        a: '',
+        b: '',
+        c: '',
+        d: '',
+        e: ''
+    }
+]
